95db3794e5f10c6ed0fe35183a195092aa9d411f
ronald vilorio <soccer23415@gmail.com>
2018-05-15 18:57:26 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -20,5 +20,5 @@ function exactMatch(drivers,attribute){
 }
 
 function exactMatchToList(drivers,attribute){
-
+  return exactMatch(drivers,attribute).map(function(driver){return driver["name"]})
 }
