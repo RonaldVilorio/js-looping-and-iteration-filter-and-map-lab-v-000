7147cd4c36e15b4c7d5ae56603a686545d15a3d8
ronald vilorio <soccer23415@gmail.com>
2018-05-15 18:31:34 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,6 +6,6 @@ function driversWithRevenueOver(drivers,revenue){
 
 function driverNamesWithRevenueOver(drivers,revenue){
   debugger
-  driversWithRevenueOver = driversWithRevenueOver(drivers,revenue)
-  driversWithRevenueOver.map(function(driver){return driver["name"]})
+  driversWithRevenueOver(drivers,revenue).map(function(driver){return driver["name"]})
+  
 }
