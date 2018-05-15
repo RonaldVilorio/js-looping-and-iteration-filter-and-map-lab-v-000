85c3cf0a86df9b5a8ed64474c306e827f3028822
ronald vilorio <soccer23415@gmail.com>
2018-05-15 18:51:41 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,10 +11,10 @@ function driverNamesWithRevenueOver(drivers,revenue){
 function exactMatch(drivers,attribute){
   debugger
   if (attribute["name"]){
-    return drivers.filter(function(driver){return driver[attribute] === attribute})
+    return drivers.filter(function(driver){return driver["name"] === attribute})
   }
   else if (attribute["revenue"]) {
-    return drivers.filter(function(driver){return driver[attribute] === attribute})
+    return drivers.filter(function(driver){return driver["revenue"] === attribute})
   }
 
 }
