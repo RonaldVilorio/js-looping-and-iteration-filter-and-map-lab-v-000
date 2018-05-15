cde34cf17f54f018daa5b2de9d847673c1d48ebf
ronald vilorio <soccer23415@gmail.com>
2018-05-15 18:08:40 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Code your solution here:
+function driversWithRevenueOver(drivers,revenue){
+    // debugger
+    return drivers.filter(function(driver){return driver["revenue"] > revenue})
+}
+function driverNamesWithRevenueOver(drivers,revenue){
+debugger
+  return drivers.map(function(d){ if (d["revenue"] > revenue){
+    return d["name"]
+  }})
+}
